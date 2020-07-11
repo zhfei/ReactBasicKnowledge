7d49df2083e6501c28ec55aad0d96c7eb07896c1
zhoufei <1174977961@qq.com>
2020-07-11 16:58:39 +0800

    feat: 子组件通过调用父组件的函数指针，来传值给父组件

--- react_app_day7/rn_app/src/components/Home.js
+++ react_app_day7/rn_app/src/components/Home.js
@@ -4,11 +4,26 @@ import SubComp from './SubComp'
 
 export default class Home extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            text:"这是在父组件中，可以通过子组件传值的属性默认值"
+        }
+    }
+
+    dataFun = (text) => {
+        this.setState({
+            text:text
+        })
+    }
+
     render(){
         return (
             <div>
                 Home -- hello
-                <SubComp text="我是参数123"/>
+                <p>Home2 子组件传值给父组件 -- {this.state.text}</p>
+                <SubComp text="我是参数123" fufun={this.dataFun}/>
             </div>
         )
     }
--- react_app_day7/rn_app/src/components/SubComp.js
+++ react_app_day7/rn_app/src/components/SubComp.js
@@ -10,7 +10,8 @@ export default class SubComp extends Component {
         super(props)
 
         this.state={
-            name:'jack'
+            name:'jack',
+            text:'这是子组件内的属性值'
         }
     }
 
@@ -25,6 +26,8 @@ export default class SubComp extends Component {
                 这是从父组件中传递过来的参数：{this.props.text}
                 <p>这个组件的作者是:{this.state.name}</p>
                 <button onClick={this.changeName.bind(this)}>点击改名字</button>
+
+                <button onClick={this.props.fufun.bind(this, this.state.text)}>点击我，将子组件属性传递给父组件</button>
             </div>
         )
     }
