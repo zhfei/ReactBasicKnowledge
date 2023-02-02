5b07e9f3ec39f77352218abd4f184d91ce9eb684
zhfei <1174977961@qq.com>
2023-02-03 00:04:16 +0800

    other: 引入antd-mobile框架

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -1,8 +1,10 @@
+import { Button } from 'antd-mobile'
 
 function App() {
   return (
     <div className="App">
         Hello React
+        <Button color='primary'>登录</Button>
     </div>
   );
 }
