6babe1ec2c685c9a204d09a9901cf00b0751af42
zhfei <1174977961@qq.com>
2023-02-03 22:39:09 +0800

    add: 添加react-router-dom@5 配置页面结构

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -1,11 +1,22 @@
 import { Button } from 'antd-mobile'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+// 导入组件
+import Home from '@/pages/Home'
+import CityList from '@/pages/CityList'
 
 function App() {
   return (
-    <div className="App">
-        Hello React
-        <Button color='primary'>登录</Button>
-    </div>
+    <Router>
+      <div className="App">
+          Hello React
+          <Button color='primary'>登录</Button>
+          <Link to='/home'>首页</Link>
+          <Link to='/citylist'>城市列表</Link>
+
+          <Route path='/home' component={Home}/>
+          <Route path='/citylist' component={CityList}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ hkzf-mobile/src/pages/CityList/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CityList extends Component {
+  render() {
+    return (
+      <div>CityList</div>
+    )
+  }
+}
