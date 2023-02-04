a255e929a42e224b5deadd69d85a997ab4ee1bae
zhfei <1174977961@qq.com>
2023-02-04 13:06:03 +0800

    style: 隐藏导航条箭头

--- hkzf-mobile/src/pages/Home/index.js
+++ hkzf-mobile/src/pages/Home/index.js
@@ -52,7 +52,7 @@ export default class Home extends Component {
       <div>
         <div className='app'>
           <div className='top'>
-            <NavBar>好客租房</NavBar>
+            <NavBar backArrow={false}>好客租房</NavBar>
           </div>
           <div className='body'>
             <Switch>
