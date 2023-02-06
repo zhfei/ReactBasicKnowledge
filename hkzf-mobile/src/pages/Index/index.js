d42b471a8134d391a8399e28525c8398d486bc38
zhfei <1174977961@qq.com>
2023-02-06 23:21:00 +0800

    add: 添加轮播图

--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react'
+import React, { useRef } from 'react'
+import { Swiper, Toast } from 'antd-mobile'
+import { SwiperRef } from 'antd-mobile/es/components/swiper'
 
-export default class Index extends Component {
-  render() {
-    return (
-      <div>index</div>
-    )
-  }
+import styles from './index.less'
+
+const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+
+const items = colors.map((color, index) => (
+  <Swiper.Item key={index}>
+    <div
+      className={styles.content}
+      style={{ background: color }}
+      onClick={() => {
+        Toast.show(`你点击了卡片 ${index + 1}`)
+      }}
+    >
+      {index + 1}
+    </div>
+  </Swiper.Item>
+))
+
+
+export default function Index() {
+  // const ref = useRef<SwiperRef>(null)
+   return (
+      <div title='基础用法'>
+        <Swiper>{items}</Swiper>
+      </div>
+   );
 }
