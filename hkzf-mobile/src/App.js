a075e098d3c8c8b93f56c674295513db67f871ee
zhfei <1174977961@qq.com>
2023-02-08 22:24:52 +0800

    feat: 轮播图展示后台数据

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -7,6 +7,7 @@ import CityList from '@/pages/CityList'
 function App() {
   return (
     <Router initialEntries={['/home']}>
+      {/* 添加重定向 */}
       <Route path='/' exact render={()=><Redirect to='/home'/>}></Route>
       <Route path='/home' component={Home}/> 
       <Route path='/citylist' component={CityList} ></Route>
--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -1,32 +1,55 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import {useHistory} from 'react-router-dom'
 import { Swiper, Toast } from 'antd-mobile'
 import { SwiperRef } from 'antd-mobile/es/components/swiper'
 
 import styles from './index.less'
-
-const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
-
-const items = colors.map((color, index) => (
-  <Swiper.Item key={index}>
-    <div
-      className={styles.content}
-      style={{ background: color, height: 200 }}
-      onClick={() => {
-        Toast.show(`你点击了卡片 ${index + 1}`)
-      }}
-    >
-      {index + 1}
-    </div>
-  </Swiper.Item>
-))
+import axios from 'axios'
 
 
 export default function Index() {
+  const [swipList, setSwipList] = useState([]);
+  const history = useHistory()
+
+  useEffect(() => {
+    getSwiperDatas();
+  }, []);
+
+  
+  function getSwiperDatas() {
+    axios.get('http://localhost:8080/home/swiper').then((res)=>{
+      console.log('结果为：',res);
+      setSwipList([...res.data.body])
+    })
+  }
+
   // const ref = useRef<SwiperRef>(null)
    return (
       <div title='基础用法' 
         style={{width: '100%', height: '100%'}}>
-        <Swiper autoplay loop autoplayInterval={1000}>{items}</Swiper>
+        <Swiper autoplay loop autoplayInterval={2000}>
+          {
+            swipList.map((item, index) => {
+              return <Swiper.Item key={item.id}>
+                      <div
+                        className={styles.content}
+                        style={{height: 200}}
+                        onClick={() => {
+                          Toast.show(`你点击了卡片 ${index + 1}`)
+                        }}
+                      >
+                        <a href="http://www.baidu.com">
+                            <img src={`http://localhost:8080${item.imgSrc}`} 
+                                alt={item.alt} 
+                                style={{width:'100%', verticalAlign: 'top'}}></img>
+                        </a>
+                        
+                      </div>
+                    </Swiper.Item>
+            })
+          }
+        </Swiper>
+        
       </div>
    );
 }
