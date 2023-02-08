7aed8f25c204d340bda6245f75398dda65e63725
zhfei <1174977961@qq.com>
2023-02-08 20:56:47 +0800

    feat: 展示轮播图

--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -10,7 +10,7 @@ const items = colors.map((color, index) => (
   <Swiper.Item key={index}>
     <div
       className={styles.content}
-      style={{ background: color }}
+      style={{ background: color, height: 200 }}
       onClick={() => {
         Toast.show(`你点击了卡片 ${index + 1}`)
       }}
@@ -24,8 +24,9 @@ const items = colors.map((color, index) => (
 export default function Index() {
   // const ref = useRef<SwiperRef>(null)
    return (
-      <div title='基础用法'>
-        <Swiper>{items}</Swiper>
+      <div title='基础用法' 
+        style={{width: '100%', height: '100%'}}>
+        <Swiper autoplay loop autoplayInterval={1000}>{items}</Swiper>
       </div>
    );
 }
