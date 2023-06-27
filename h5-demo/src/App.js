7d246d3b63b0774df632a7252f81d0a1fbf08555
zhfei <1174977961@qq.com>
2023-06-27 22:08:10 +0800

    feat: 完善h5容器方法注入逻辑

--- h5-demo/src/App.js
+++ h5-demo/src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[name, setName] = useState('')
+ 
+  // 0.公共
+  //原生发消息给JS，JS的回调
+  window.qrResult = (res)=>{
+    setName(res)
+    return '-------: '+res
+  }
+  // scheme拦截
+  const localPostion = () => {
+    window.location.href = 'position://localPosition?name=jack&age=20'
+  }
+ 
+  // 1.WKWebView的交互
+  //js发消息给原生
+  const qrAction = () => {
+    window.webkit.messageHandlers.QRCodeScan.postMessage({"name":"value"})
+  }
+ 
+  // 2.UIWebView的交互
+  //js发消息给原生
+  const qrActionOnAppModel = () => {
+    const res = window.appModel.QRCodeScan({"name":"value"})
+    alert(res.name)
+  }
+  const showAlert = () => {
+    window.showAlert()
+  }
+ 
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +48,22 @@ function App() {
         >
           Learn React
         </a>
+ 
+        <div>------------------公共------------------</div>
+        <a href='position://abc?name=jack' style={{color:'white'}}>scheme拦截1：定位</a>
+        <button onClick={localPostion}>scheme拦截2</button>
+        <div>
+         原生执行代码的结果：{name}
+        </div>
+ 
+        <div>------------------WKWebView------------------</div>
+        <button onClick={qrAction}>点击扫描</button>
+         
+ 
+        <div>------------------UIWebView------------------</div>
+        <button onClick={qrActionOnAppModel}>点击扫码</button>
+        <button onClick={showAlert}>弹窗</button>
+ 
       </header>
     </div>
   );
