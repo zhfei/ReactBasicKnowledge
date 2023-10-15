10049c0c6b711c15aa0848b67c77c44056dfbb43
zhfei <1174977961@qq.com>
2023-10-15 21:01:40 +0800

    add:添加loader处理器

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,6 +1,14 @@
 import $ from 'jquery'
+import "./css/index.scss"
 
 $(function () {
     $('li:odd').css('backgroundColor','pink')
     $('li:even').css('backgroundColor','lightblue')
 })
+
+
+class Person {
+    static info = {name: 'jack'}
+}
+
+console.log(Person.info)
