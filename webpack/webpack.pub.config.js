3a7a541ae7c1941efdb81e4628c6d59be21db16d
zhfei <1174977961@qq.com>
2023-10-15 21:30:16 +0800

    add: 添加pub发布短命令

--- webpack/webpack.pub.config.js
+++ webpack/webpack.pub.config.js
@@ -30,7 +30,7 @@ module.exports = {
         rules: [
             { test: /\.css$/, use:['style-loader', 'css-loader'] },
             { test: /\.scss$/, use:['style-loader', 'css-loader', 'sass-loader'] },
-            { test: /\.(png|gif|bmp|jpg)$/, use: 'url-loader?limit=500000' },
+            { test: /\.(png|gif|bmp|jpg)$/, use: 'url-loader?limit=5000&name=[hash:8]-[name].[ext]' },
             // { test: /\.js$/, use:'babel-loader', exclude: /node_modules/ },
         ]
     }
