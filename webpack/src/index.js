ad2dac8baa2e1afe9f8ad3fbb77c3d271ce7f055
zhfei <1174977961@qq.com>
2023-10-16 23:41:13 +0800

    add:添加抽象配置

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import "./css/index.scss"
+//发布思路：bundle.js中只存放自己的代码，第三方包的代码都抽离到另外的JS包中
 
 $(function () {
     $('li:odd').css('backgroundColor','pink')
--- webpack/webpack.pub.config.js
+++ webpack/webpack.pub.config.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
     mode: 'production',
     //项目的入口与出口设置
     entry: path.join(__dirname, './src/index.js'),
+    //app和vendors的key名称都是自己自定义的，随便取的
+    // entry: {
+    //     app: path.join(__dirname, './src/index.js'),
+    //     vendors: ['jquery'] //把要抽离的第三方包放到这个数组中
+    // }, 
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
@@ -22,8 +28,35 @@ module.exports = {
             template: path.join(__dirname, './src/index.html'),
             filename: 'index.html'
         }),
+        /*
+            每次构建产物都要重新创建dist目录保存产物
+        */
         new CleanWebpackPlugin({cleanAfterEveryBuildPatterns:['dist']})
     ],
+
+    /*
+        抽离第三方包名称
+        webpack3的在plugins数组中添加new webpack.optimize.CommonsChunkPlugin配置方式已经废弃，要在下面的方法进行实现
+        webpack3实现方法
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors', //指定抽离的入口
+            filename: 'vendors.js' //抽离的所有第三方包的结果包名称。
+        })
+        webpack5设置无效，先注释
+    */
+    // optimization: {
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             commons: {
+    //                 test: /[\\/]node_modules[\\/]/,
+    //                 name: 'vendors',
+    //                 chunks: 'all',
+    //             },
+    //         }
+    //     }
+
+    // },
+
     /*
         webpack默认只能打包处理以.js结尾的模块。其他非.js后缀名结尾的模块，webpack默认是不处理的。
         需要调用loader加载器才可以正常打包，loader加载器的作用：协作webpack打包处理约定的文件模块。比如：css-loader: 可以打包处理.css相关的文件。
