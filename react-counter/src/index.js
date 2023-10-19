a3721594aaacb4a3bce9c5de06dc86530cd22f36
zhfei <1174977961@qq.com>
2023-10-19 09:14:21 +0800

    add: 添加Counter计数器

---
+++ react-counter/src/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentList from "./components/comment/CommentList.jsx";
+
+
+import "./css/commentList.css"
+
+
+
+ReactDOM.render(<div>
+    <CommentList></CommentList>
+</div>, document.getElementById("root"))
+
+
